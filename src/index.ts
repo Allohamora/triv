e2ab2f17c97961e0c15d8333a06d849f7005bb30
Allohamora <herman2000@yandex.ru>
2020-06-25 17:45:44 +0300

    Added skip and stop commands

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,11 @@ interface GlobalState {
     [id: string]: {
         count: number,
         isStarted: boolean,
+        isSkiped: boolean,
+
         connection: Discord.VoiceConnection,
         voice: Discord.VoiceChannel,
+        stream: Discord.StreamDispatcher | null,
     }
 }
 // app state
@@ -29,183 +32,264 @@ const preparation = async() => await mongoose.connect(MONGO_URI, {
 // list of emoji
 const emoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣"];
 
+// list of commands;
+const commands = {
+    play: `${PREFIX}play`,
+    skip: `${PREFIX}skip`,
+    stop: `${PREFIX}stop`,
+    add: `${PREFIX}add`,
+}
+
 // loading from apple music to db
 let isLoading: boolean = false;
 
-// create regexp with prefix
-const withPrefix = (pattern: string, flags?: string) => new RegExp( "^" + PREFIX + pattern, flags); 
+// music error handler
+const errorHandler = (msg: Discord.Message) => {
+    const { id } = msg.member.guild;
 
+    msg.reply("error with bot!");
+
+    if( globalState[id] ) {
+        globalState[id].voice.leave();
+
+        delete globalState[id];
+    }
+}
 
 const client = new Discord.Client();
+
+// play command
 client.on("message", async msg => {
     const { content } = msg;
 
-    // play command
-    if( withPrefix("play( \\d+)?").test(content) ) {
-        try{
-            const { id } = msg.member.guild;
+    // if not play command
+    if( !new RegExp(`${commands.play}( \\d+)?`).test(content) ) return;
 
-            if( !globalState[id] ) {
-                const match = content.match(/\d+/);
-    
-                globalState[id] = {
-                    isStarted: false,
-                    count: match ? Number(match[0]) : 3,
-                    voice: msg.member.voice.channel,
-                    connection: await msg.member.voice.channel.join(),
-                }
+    try{
+        const { id } = msg.member.guild;
+
+        if( !globalState[id] ) {
+            const match = content.match(/\d+/);
+
+            globalState[id] = {
+                isStarted: false,
+                isSkiped: false,
+                count: match ? Number(match[0]) : 3,
+
+                voice: msg.member.voice.channel,
+                connection: await msg.member.voice.channel.join(),
+                stream: null,
             }
-    
-            if( globalState[id].isStarted ) return msg.reply("game already started!");
-    
-            globalState[id].isStarted = true;
-    
-            // get quize
-            const quize = await getQuize();
-    
-            //get random number for right quize
-            const random = Math.floor( Math.random() * 4 );
-    
-            // select quize
-            const song = quize[random];
-    
-            //get read stream from url
-            const stream = await getByLink(song.src);
-    
-            // get voice channel, and voice connection
-            const { voice, connection } = globalState[id];
-    
-            // create embed msg
-            const embed = new Discord.MessageEmbed()
+        }
+
+        const state = globalState[id];
+
+        if( state.isStarted ) return msg.reply("game already started!");
+
+        state.isStarted = true;
+
+        // get quize
+        const quize = await getQuize();
+
+        //get random number for right quize
+        const random = Math.floor( Math.random() * 4 );
+
+        // select quize
+        const song = quize[random];
+
+        //get read stream from url
+        const stream = await getByLink(song.src);
+
+        // get voice channel, and voice connection
+        const { voice, connection } = state;
+
+        // create embed msg
+        const embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle(song.author)
             .setFooter("powered by apple music!")
             .setDescription(quize.map( ({title}, index) => emoji[index] + "\t\t" + title ).join("\n \n"))
             .setTimestamp()
-    
-            // get link to embed message
-            const message = await msg.channel.send(embed);
-    
-            // pick right emoji
-            const rightEmoji = emoji[random];
-    
-            // add react to message
-            emoji.forEach( emo => {
-                message.react(emo);
-            } );
-    
-            // wait all reactions
-            const reactions = message.awaitReactions(() => true, { time: 28000 });
-    
-            stream.on("end", () => {
-    
+
+        // get link to embed message
+        const message = await msg.channel.send(embed);
+
+        // pick right emoji
+        const rightEmoji = emoji[random];
+
+        // add react to message
+        emoji.forEach( emo => {
+            message.react(emo);
+        } );
+
+        // wait all reactions
+        const reactions = message.awaitReactions(() => true, { time: 28000 });
+
+        // check count 
+        const checkCount = () => setTimeout( async() => {
+            state.count--;
+
+            // if count > 0
+            if( state.count ) {
+                // reset started
+                state.isStarted = false;
+                state.isSkiped = false;
+                
+                // send play command
+                const msg = await message.channel.send(commands.play);
+
+                // delete play command
+                await msg.delete();
+            } else {
+
+                // if count < 0, leave from voice channel
+                delete globalState[id];
+                voice.leave();
+            }
+        }, 3000 );
+
+
+        // edit message
+        const editMessage = (extra: string = "") => {
+            embed.setTitle(song.full);
+            embed.setDescription(rightEmoji + "\t\t" + song.title + "\n\n" + extra);
+            embed.setURL(song.link);
+            message.edit(embed);
+        }
+
+        // adding stream 
+        state.stream = connection.play( stream )
+            .on("finish", () => {
+
+                if( state.isSkiped ) {
+                    editMessage("skiped!")
+                    checkCount();
+                    return;
+                }
+
                 // check reactions
                 reactions.then( collected => {
-                    // all reactions without bot
+
+                    // create notifications for users
                     const arr = [...collected.get(rightEmoji).users.cache].map( ([id, user]) => {
                         if( id === client.user.id ) return "";
                         return `<@${id}>`
                     });
-    
-                    // edit embed
-                    embed.setTitle(song.full);
-                    embed.setDescription(rightEmoji + "\t\t" + song.title + "\n\n" + arr.join(""));
-                    embed.setURL(song.link);
-                    message.edit(embed);
-    
+
+                    // view result
+                    editMessage(arr.join(""));
+
                     // check count 
-                    setTimeout( async() => {
-                        globalState[id].count--;
-    
-                        // if count > 0
-                        if( globalState[id].count ) {
-                            // reset started
-                            globalState[id].isStarted = false;
-                            
-                            // send play command
-                            const msg = await message.channel.send(`${PREFIX}play`);
-    
-                            // delete play command
-                            await msg.delete();
-                        } else {
-    
-                            // if count < 0, leave from voice channel
-                            delete globalState[id];
-                            voice.leave();
-                        }
-                    }, 3000 );
+                    checkCount();
                 });
-            });
-    
-            stream.on("error", () => {
+            })
+            .on("error", () => {
                 // if error leave from voice channel
                 voice.leave();
             })
-    
-    
-            // play music intro voice channel
-            connection.play( stream );
-        } catch(e) {
-            const { id } = msg.member.guild;
 
-            msg.reply("error with bot!");
-
-            if( globalState[id] ) {
-                globalState[id].voice.leave();
-            }
-        }
+    } catch(e) {
+        errorHandler(msg)
     }
+});
 
-    // add album command
-    if( withPrefix("add https://music.apple.com/ru/album.+?").test(content) ) {
-        try{
-            //regexp for right link
-            const regexp = /https:\/\/music.apple.com\/ru\/album\/.+/;
+// add command
+client.on("message", async msg => {
+    const { content } = msg;
 
-            // if not right
-            if( !regexp.test(content) ) return msg.reply("incorrect url!");
+    // if not add album command
+    if( !new RegExp(`${commands.add} https://music.apple.com/ru/album.+?`).test(content) ) return;
 
-            // delete selected song id
-            const link = content.match(regexp)[0].replace(/\?i=.*/, "");
+    try {
+        //regexp for right link
+        const regexp = /https:\/\/music.apple.com\/ru\/album\/.+/;
 
-            // if already in db
-            const inDB = await Song.findOne({ link });
+        // if not right
+        if( !regexp.test(content) ) return msg.reply("incorrect url!");
 
-            // if bot loading album to db
-            if( isLoading ) return msg.reply("please wait!");
-            if( inDB ) return msg.reply("already in db!");
+        // delete selected song id
+        const link = content.match(regexp)[0].replace(/\?i=.*/, "");
 
-            // start loading
-            isLoading = true;
+        // if already in db
+        const inDB = await Song.findOne({ link });
 
-            // wait parse album
-            const links = await parseAppleMusic(link);
+        // if bot loading album to db
+        if( isLoading ) return msg.reply("please wait!");
+        if( inDB ) return msg.reply("already in db!");
+
+        // start loading
+        isLoading = true;
+
+        // wait parse album
+        const links = await parseAppleMusic(link);
+    
+        // create result from parse data
+        const result = links.map( ({ src, title: full }) => {
+            const splited = full.split(" - ");
         
-            // create result from parse data
-            const result = links.map( ({ src, title: full }) => {
-                const splited = full.split(" - ");
-            
-                const [title, author] = splited;
-                let album = splited[2];
-            
-                if( splited[3] ) album += " - " + splited[3];
+            const [title, author] = splited;
+            let album = splited[2];
         
-                return { title, author, album, src, full, link };
-            } );
+            if( splited[3] ) album += " - " + splited[3];
+    
+            return { title, author, album, src, full, link };
+        } );
 
-            // insert all result into db
-            await Song.insertMany(result);
+        // insert all result into db
+        await Song.insertMany(result);
 
-            // answer
-            msg.reply("success");
+        // answer
+        msg.reply("success");
 
-            // set loading off
-            isLoading = false;
-        }catch(e) {
-            msg.reply("error with bot!");
-        }
+        // set loading off
+        isLoading = false;
+    } catch(e) {
+        msg.reply("error with adding to db!");
     }
-})
+});
+
+// skip command
+client.on("message", async msg => {
+    const { content } = msg;
+
+    if( !new RegExp(commands.skip).test(content) ) return;
+
+    try {
+        const { id } = msg.member.guild;
+
+        if( !globalState[id] ) return;
+
+        // set skiped 
+        globalState[id].isSkiped = true;
+
+        // invoke finish event
+        globalState[id].stream?.end();
+    } catch (e) {
+        errorHandler(msg);
+    }
+});
+
+// stop command
+client.on("message", async msg => {
+    
+    const { content } = msg;
+    if( !new RegExp(commands.stop).test(content) ) return;
+
+    try {
+        const { id } = msg.member.guild;
+
+        const state = globalState[id];
+
+        if( !state ) return;
+
+        // set state for skip command
+        state.count = 1;
+        const message = await msg.channel.send(commands.skip);
+        await message.delete();
+    } catch (e) {
+        errorHandler(msg);
+    }
+});
 
 client.on("ready", () => console.log(`Started as ${client.user.tag}`));
 preparation().then( () => client.login(TOKEN) );
